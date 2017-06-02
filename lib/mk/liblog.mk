c838f2256652ccd74159991f041770422fcbffb3
Alexander Senier <senier@secure-systems-engineering.de>
2017-06-02 23:42:49 +0200

    Build liblog from Android build files

--- lib/mk/liblog.mk
+++ lib/mk/liblog.mk
@@ -1,20 +1,9 @@
-CORE_DIR   := $(call select_from_ports,liblog)/system/core
-LIBLOG_DIR := $(CORE_DIR)/liblog
+ANDROID_BASEDIR    = $(call select_from_ports,liblog)/system/core
+ANDROID_INCLUDES   = include liblog liblog/include
+ANDROID_SOURCES    = liblog
+ANDROID_BUILDFILES = liblog/Android.bp
+ANDROID_SECTIONS   = /cc_library[@name=liblog] /cc_library[@name=liblog]/target/host
 
-SRC_C = log_event_list.c log_event_write.c logger_write.c config_write.c \
-		config_read.c logger_name.c logger_lock.c fake_log_device.c fake_writer.c \
-		local_logger.c
+include $(call select_from_repositories,lib/mk/android.inc)
 
-SRC_CC = log_ratelimit.cpp 
-
-CC_OPT  += -DFAKE_LOG_DEVICE="1"
-CPP_OPT += $(CC_OPT)
-
-vpath % $(LIBLOG_DIR)
-
-INC_DIR += $(CORE_DIR)/include
-INC_DIR += $(LIBLOG_DIR)
-INC_DIR += $(LIBLOG_DIR)/include
-
-LIBS += libc cxx
-SHARED_LIB  = yes
+CC_OPT += -Wno-unused-variable -Wno-unused-function
