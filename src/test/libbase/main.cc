c4019f0af725fa4237792e1be042faf9856b4bfc
Alexander Senier <senier@componolit.com>
2017-07-05 23:58:03 +0200

    libbase: Suppress unsupported tests

--- src/test/libbase/main.cc
+++ src/test/libbase/main.cc
@@ -13,8 +13,14 @@ extern "C" int main(int argc, char const **argv);
 void Libc::Component::construct(Libc::Env &env)
 {
 	Libc::with_libc([&] {
-		char const *argv[] = { "/bin/test_libbase", 0 };
-		genode_argc = 1;
+
+		char const *argv[] = {
+			"/bin/test_libbase",
+			"--gtest_filter=-logging.LOG_STREAM_FATAL_WITHOUT_ABORT_*:logging.LOG_STREAM_*:logging.LOG_FATAL_WITHOUT_ABORT_*:logging.LOG_*_*:logging.LOG_complex_param:logging.*LOG_does_not_clobber_errno:logging.LOG_does_not_have_dangling_if:logging.PLOG_FATAL_WITHOUT_ABORT_*:logging.PLOG_*_*:logging.UNIMPLEMENTED",
+			0
+		};
+
+		genode_argc = 2;
 		genode_argv = argv;
 
 		setprogname (genode_argv[0]);
