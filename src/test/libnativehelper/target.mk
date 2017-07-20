d5787b9bd07d19d9687a3408de42e65fe9a65d1e
Alexander Senier <senier@componolit.com>
2017-07-21 01:00:01 +0200

    Port libnativhelper unit tests, closes #11

--- src/test/libnativehelper/target.mk
+++ src/test/libnativehelper/target.mk
@@ -1,3 +1,10 @@
-TARGET = test-libnativehelper
-LIBS   = libnativehelper
-SRC_CC = main.cc
+TARGET = libnativehelper_test
+
+ANDROID_PORT	 = libnativehelper
+ANDROID_DIR		 = libnativehelper/tests
+ANDROID_SECTIONS = /cc_test[@name=JniInvocation_test]
+
+include $(call select_from_repositories,lib/mk/android-prg.inc)
+
+SRC_CC += main.cc
+LIBS   += gtest posix
