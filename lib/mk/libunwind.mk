8bfe5d126210bda3400ca24fef81cb3e3cb21d54
Alexander Senier <senier@componolit.com>
2017-08-06 15:18:46 +0200

    libunwind: Filter -Wno-#pragma-messages, unsupported by Genode toolchain

--- lib/mk/libunwind.mk
+++ lib/mk/libunwind.mk
@@ -1,5 +1,6 @@
 ANDROID_PORT = libunwind
 ANDROID_DIR = libunwind
 ANDROID_SECTIONS = /cc_library[@name=libunwind] /cc_library[@name=libunwind]/arch/$(ANDROID_ARCH)
+ANDROID_EXCLUDE_OPT = -Wno-\#pragma-messages
 
 include $(call select_from_repositories,lib/mk/android-lib.inc)
