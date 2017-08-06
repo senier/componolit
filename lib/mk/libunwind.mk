e1afe67c91c37bc514f0fa37fe7a4a1b27943958
Alexander Senier <senier@componolit.com>
2017-08-06 21:57:31 +0200

    libunwind: Provide ucontext.h

--- lib/mk/libunwind.mk
+++ lib/mk/libunwind.mk
@@ -4,3 +4,6 @@ ANDROID_SECTIONS = /cc_library[@name=libunwind] /cc_library[@name=libunwind]/arc
 ANDROID_EXCLUDE_OPT = -Wno-\#pragma-messages
 
 include $(call select_from_repositories,lib/mk/android-lib.inc)
+
+# Required for custom ucontext.h which includes sys/ucontext.h as provided by libc
+INC_DIR += $(call select_from_repositories,src/lib/libunwind)
