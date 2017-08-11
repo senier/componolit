bbb0fdf3942885ddd9356019bd99dfc176327315
Alexander Senier <senier@componolit.com>
2017-08-11 10:52:48 +0200

    libnativehelper: Adapt to o-preview

--- lib/mk/libnativehelper.mk
+++ lib/mk/libnativehelper.mk
@@ -1,6 +1,6 @@
 ANDROID_PORT     = libnativehelper
 ANDROID_DIR      = libnativehelper
-ANDROID_SECTIONS = /cc_library_headers[@name=jni_headers] /cc_library[@name=libnativehelper]
+ANDROID_SECTIONS = /cc_library[@name=libnativehelper]
 ANDROID_INCLUDES = include/nativehelper
 
 include $(call select_from_repositories,lib/mk/android-lib.inc)
