d7a6492ec1e72832620b1a4542795c08eb1a97ff
Alexander Senier <senier@componolit.com>
2017-08-11 18:51:57 +0200

    Download Genode toolchain

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -ev
 
+curl -O -J -L https://sourceforge.net/projects/genode/files/genode-toolchain/17.05/genode-toolchain-17.05-x86_64.tar.xz
+sudo tar -xf genode-toolchain-17.05-x86_64.tar.xz -C /
+
+genode-x86-gcc --version
+
 echo "Start $(date)"
 echo "Args: $*"
 echo "Uname: $(uname -a)"
