6658060dbf86de4a5c8cd087c6a3dab61e82ddfb
Alexander Senier <senier@componolit.com>
2017-08-30 00:52:18 +0200

    Generate and link operator implementations with libart

--- lib/mk/libart.mk
+++ lib/mk/libart.mk
@@ -5,13 +5,24 @@ ANDROID_SOURCES = runtime
 ANDROID_BUILDFILES = test/Android.bp build/Android.bp runtime/Android.bp
 ANDROID_SECTIONS = \
 	/art_cc_library[@name=libart] \
-	/art_cc_library[@name=libart]/arch/$(ANDROID_ARCH)
+	/art_cc_library[@name=libart]/arch/$(ANDROID_ARCH) \
+	/gensrcs[@name=art_operator_srcs]
 
 include $(call select_from_repositories,lib/mk/libart-defaults.inc)
 include $(call select_from_repositories,lib/mk/android-lib.inc)
 
+BASEDIR = $(call select_from_ports,libart)/art/runtime
+GENTOOL = $(BASEDIR)/$($(ANDROID_NAME)_TOOL_FILES)
+HEADERS = $(addprefix $(BASEDIR)/,$(filter %.h,$($(ANDROID_NAME)_SRCS)))
+OUTPUT  = $($(ANDROID_NAME)_OUTPUT_EXTENSION)
+GENDIR  = $(LIB_CACHE_DIR)/$(ANDROID_BUILDTYPE)/$(ANDROID_NAME)
 
+$(GENDIR)/$(OUTPUT): $(HEADERS)
+	$(VERBOSE)$(GENTOOL) $(HEADERS) > $@.tmp
+	$(VERBOSE)mv $@.tmp $@
 
+vpath %.cc $(GENDIR)
+SRC_C += $(OUTPUT)
 
 # Position independent library
 CC_OPT += -fPIC
