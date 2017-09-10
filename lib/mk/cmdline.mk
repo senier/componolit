096a76b98f49f448e356ace64d1b38961886124d
Alexander Senier <senier@componolit.com>
2017-09-11 01:11:52 +0200

    Import cmdline header

--- lib/mk/cmdline.mk
+++
@@ -1,6 +0,0 @@
-ANDROID_PORT = libart
-ANDROID_DIR = art
-ANDROID_BUILDFILES = build/Android.bp test/Android.bp cmdline/Android.bp
-ANDROID_SECTIONS = /art_cc_test[@name=art_cmdline_tests]
-
-include $(call select_from_repositories,lib/mk/android-lib.inc)
