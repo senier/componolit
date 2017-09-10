8eb8ec56b637a34d552658a09de03fe36c7653d2
Alexander Senier <senier@componolit.com>
2017-09-10 22:05:51 +0200

    Implement dummy backtrace library, closes #63

--- lib/mk/libbacktrace.mk
+++ lib/mk/libbacktrace.mk
@@ -1,1 +1,6 @@
-TARGET = libbacktrace
+INC_DIR += $(call select_from_ports,core)/system/core/include
+vpath % $(call select_from_repositories,src/lib/libbacktrace)
+
+LIBS += stdcxx
+SRC_CC += backtrace.cc
+SHARED_LIB = yes
