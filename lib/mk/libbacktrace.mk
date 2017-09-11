02bab00896c80fc9799e755ed4f216dee88b9aec
Alexander Senier <senier@componolit.com>
2017-09-11 14:06:36 +0200

    Add dummy BacktraceMap::Create

--- lib/mk/libbacktrace.mk
+++ lib/mk/libbacktrace.mk
@@ -2,5 +2,5 @@ INC_DIR += $(call select_from_ports,core)/system/core/include
 vpath % $(call select_from_repositories,src/lib/libbacktrace)
 
 LIBS += stdcxx
-SRC_CC += backtrace.cc
+SRC_CC += Backtrace.cc BacktraceMap.cc
 SHARED_LIB = yes
