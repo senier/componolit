07bc9f86802bdcbd3533f2bde66cefd83533b1a2
Alexander Senier <senier@componolit.com>
2017-09-25 15:45:31 +0200

    Add not-implemented warnings to fake libs

--- src/lib/libbacktrace/Backtrace.cc
+++ src/lib/libbacktrace/Backtrace.cc
@@ -1,4 +1,10 @@
+// Genode includes
+#include <base/log.h>	// For warning()
+
+// Android includes
 #include <backtrace/Backtrace.h>
+
+// libc include
 #include <sys/types.h>
 
 class BacktraceFake : public Backtrace {
@@ -16,22 +22,26 @@ protected:
 
 bool BacktraceFake::Unwind(size_t num_ignore_frames, ucontext_t* context)
 {
+	Genode::warning(__func__, " not implemented");
 	return false;
 }
 
 bool BacktraceFake::ReadWord(uintptr_t ptr, word_t* out_value)
 {
 	out_value = 0;
+	Genode::warning(__func__, " not implemented");
 	return false;
 }
 
 size_t BacktraceFake::Read(uintptr_t addr, uint8_t* buffer, size_t bytes)
 {
+	Genode::warning(__func__, " not implemented");
 	return 0;
 }
 
 std::string BacktraceFake::GetFunctionNameRaw(uintptr_t pc, uintptr_t* offset)
 {
+	Genode::warning(__func__, " not implemented");
 	return "";
 }
 
@@ -45,30 +55,36 @@ Backtrace::~Backtrace()
 
 Backtrace* Backtrace::Create(pid_t pid, pid_t tid, BacktraceMap* map)
 {
+	Genode::warning(__func__, " not implemented");
 	return new BacktraceFake(pid, tid, map);
 }
 
 void Backtrace::FillInMap(uintptr_t pc, backtrace_map_t* map)
 {
+	Genode::warning(__func__, " not implemented");
 	map = NULL;
 }
 
 std::string Backtrace::FormatFrameData(size_t frame_num)
 {
+	Genode::warning(__func__, " not implemented");
 	return "<dummy>";
 }
 
 std::string Backtrace::FormatFrameData(const backtrace_frame_data_t* frame)
 {
+	Genode::warning(__func__, " not implemented");
 	return "<dummy>";
 }
 
 std::string Backtrace::GetFunctionName(uintptr_t pc, uintptr_t* offset)
 {
+	Genode::warning(__func__, " not implemented");
 	return "";
 }
 
 bool Backtrace::VerifyReadWordArgs(uintptr_t ptr, word_t* out_value)
 {
+	Genode::warning(__func__, " not implemented");
 	return false;
 }
--- src/lib/libfakeatomic/fakeatomic.cc
+++ src/lib/libfakeatomic/fakeatomic.cc
@@ -1,10 +1,23 @@
+// Genode includes
+#include <base/log.h>
+
+// stdcxx includes
 #include <atomic>
+
+// libc includes
 #undef ATOMIC_VAR_INIT
 #include <stdatomic.h>
 
 namespace std {
 
-void __atomic_thread_fence (memory_order mo __attribute__((unused))) { };
-void __atomic_signal_fence (memory_order mo __attribute__((unused))) { };
+void __atomic_thread_fence (memory_order mo __attribute__((unused)))
+{
+	Genode::warning(__func__, " not implemented");
+};
+
+void __atomic_signal_fence (memory_order mo __attribute__((unused)))
+{
+	Genode::warning(__func__, " not implemented");
+};
 
 }
