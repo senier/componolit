cebd66ed1c5defcfd6b3b5e06743a10cea7fe810
Alexander Senier <senier@componolit.com>
2017-09-26 22:48:52 +0200

    Dummy signal handler and init

--- src/lib/libart/runtime_genode.cc
+++ src/lib/libart/runtime_genode.cc
@@ -1,17 +1,16 @@
 #include "runtime.h"
 #include "runtime_common.h"
 
+#include <base/log.h>
+
 namespace art {
 
 void HandleUnexpectedSignalGenode(int signal_number, siginfo_t* info, void* raw_context) {
-	HandleUnexpectedSignalCommon(signal_number, info, raw_context, /* running_on_linux */ false);
-	exit(EXIT_FAILURE);
+	Genode::warning(__func__, " not implemented");
 }
 
 void Runtime::InitPlatformSignalHandlers() {
-  InitPlatformSignalHandlersCommon(HandleUnexpectedSignalGenode,
-                                   nullptr,
-                                   /* handle_timeout_signal */ true);
+	Genode::warning(__func__, " not implemented");
 }
 
 }
