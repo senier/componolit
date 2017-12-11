48ffceee8df5f91acc703e19a7a719671848d2af
Johannes Kliemann <kliemann@componolit.com>
2017-12-11 15:38:11 +0100

    add join test

---
+++ src/test/threads/join/main.cc
@@ -0,0 +1,32 @@
+
+#include <libc/component.h>
+#include <base/log.h>
+#include <timer_session/connection.h>
+
+#include <thread>
+
+static void test(int *x, Timer::Connection *_t)
+{
+    Genode::log("threads join: ", __func__);
+    _t->msleep(5000);
+    *x = 1;
+}
+
+struct Main{
+    
+    Timer::Connection _timer;
+
+    Main(Genode::Env &env) : _timer(env)
+    {
+        Genode::log("threads join: ", __func__);
+        int x = 0;
+        std::thread t(test, &x, &_timer);
+        t.join();
+        Genode::log("threads join: ", "finished ", x);
+    }
+};
+
+void Libc::Component::construct(Libc::Env &env)
+{
+   static Main main(env); 
+}
