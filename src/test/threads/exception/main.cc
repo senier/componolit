9236fda87795d6a6acf4e6a83a49b4a7fc15cd4f
Johannes Kliemann <kliemann@componolit.com>
2017-12-21 17:15:58 +0100

    extend exception test

--- src/test/threads/exception/main.cc
+++ src/test/threads/exception/main.cc
@@ -2,12 +2,20 @@
 #include <libc/component.h>
 #include <base/log.h>
 
+class Test_exception : public Genode::Exception {};
+
 struct Main{
 
-    void __attribute__((noinline)) throw_exception(void)
+    void __attribute__((noinline)) throw_scalar_exception(void)
     {
         Genode::log("threads exception: ", __func__);
         throw 0;
+    }
+
+    void __attribute__((noinline)) throw_class_exception(void)
+    {
+        Genode::log("threads exception: ", __func__);
+        throw Test_exception();
     }
 
     Main(Genode::Env &env)
@@ -15,11 +23,18 @@ struct Main{
         Genode::log("threads exception: ", __func__);
         try{
             Genode::log("threads exception: try");
-            throw_exception();
+            throw_scalar_exception();
         }catch(...){
             Genode::log("threads exception: catched");
         }
-        throw_exception();
+        
+        try{
+            Genode::log("threads exception: try");
+            throw_class_exception();
+        }catch(...){
+            Genode::log("threads exception: catched");
+        }
+        throw_class_exception();
     }
 };
 
